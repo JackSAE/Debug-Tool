9e8dd2da6ef687f612153f9669d64c4c49072457
JackSAE <1005787@student.sae.edu.au>
2016-12-13 22:17:29 +1000

    Added Code to View All Scripts + variables

--- Assets/Debug_Tool.cs
+++ Assets/Debug_Tool.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using System.Linq;
+using System.Reflection;
+
 
 /*
     Aim For Debug Tool:
@@ -16,109 +19,102 @@ using System;
 ##############################
 */
 
-
-public class Debug_Tool_Reader : MonoBehaviour
-{
-    //simple struct to store the type and name of variables
-    public struct Variable
-    {
-        public string name;
-        public Type type;
-    }
-
-    public object getValue(string name)
-    {
-        return this.GetType().GetProperty(name).GetValue(this, null);
-    }
-
-    //Function to Get variables
-
-
-    //Function to Set Variables 
-
-
-}
-
 public class Debug_Tool : MonoBehaviour {
 
+    
+    int w = Screen.width, h = Screen.height;
+
+
     float deltaTime = 0.0f;
+
     public bool GUIEnabled = false;
     public bool GUIGameObjectShow = false;
-    public string hitGameObject;
+
+    public string hitGameObjectName;
+    MonoBehaviour[] scriptsOnTarget;
     public Transform hitGameObjectTransform;
+
     public string objectValues;
     public string scriptObjectName;
-
+    public string gameObjectTransformEdit;
     public string gameObjectType;
 
     public Vector3 spawnPos;
 
-    List<string> gameObjectList = new List<string>();
+    //getting Scripts from gameObjects
+    public List<string> gameObjectList = new List<string>();
+    public List<string> gamevariablesList = new List<string>();
 
     public GameObject[] gameObjectsToSpawn;
 
-    public List<Component> components;
-    public bool cacheComponentsNow;
+    FieldInfo[] myFieldInfo;
+    Type classType;
+    [SerializeField] string className;
 
-    Debug_Tool_Reader debug_Tool_Reader;
-
-    int x, y = 10;
-    public float width = 100;
-    public float height = 20;
-
-    void Start()
-    {
-      
-    }
-    void Awake()
-    {
-        if (!Application.isPlaying)
-            CacheComponents();
-    }
 
     void Update()
     {
         deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
 
+        //Toggle Debug Menu
         if (Input.GetKeyUp(KeyCode.BackQuote))
         {
             GUIEnabled = !GUIEnabled;
         }
 
-        if(GUIEnabled && Input.GetMouseButton(0))
+        #region RayCasting 
+        if (GUIEnabled && Input.GetMouseButton(0))
         {
             RaycastHit hitInfo = new RaycastHit();
 
             bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
             if (hit)
             {
-                hitGameObject = hitInfo.transform.gameObject.name;
+                hitGameObjectName = hitInfo.transform.gameObject.name;
                 hitGameObjectTransform = hitInfo.transform;
                 gameObjectType = hitInfo.transform.gameObject.GetType().Name;
                 //scriptObjectName = hitInfo.transform.
                 GUIGameObjectShow = true;
+                scriptsOnTarget = hitInfo.transform.gameObject.GetComponents<MonoBehaviour>();
+
+            
             }
         }
+        #endregion
     }
 
-    void CacheComponents()
+    #region  Turning a string to vector -> Used to move a gameObject around
+    public static Vector3 StringToVector3(string sToVector)
     {
-        components = new List<Component>();
-        foreach (var component in GetComponents<Component>())
+        // Remove the parentheses
+        if (sToVector.StartsWith("(") && sToVector.EndsWith(")"))
         {
-            if (component != this) components.Add(component);
+            sToVector = sToVector.Substring(1, sToVector.Length - 2);
         }
+
+        // split the items
+        string[] sToArray = sToVector.Split(',');
+
+        // store as a Vector3
+        Vector3 result = new Vector3(
+            float.Parse(sToArray[0]),
+            float.Parse(sToArray[1]),
+            float.Parse(sToArray[2]));
+
+        return result; //Feels Good Man
     }
+    #endregion
+
 
     void OnGUI()
     {
+        //Checks if "~" has been pressed
         if(GUIEnabled)
         {
 
-            int w = Screen.width, h = Screen.height;
-
             GUIStyle style = new GUIStyle();
 
+            #region FPS GUI 
             Rect rect = new Rect(0, 0, w , h * 2 / 100);
             style.alignment = TextAnchor.UpperLeft;
             style.fontSize = h * 4 / 100;
@@ -127,21 +123,89 @@ public class Debug_Tool : MonoBehaviour {
             float fps = 1.0f / deltaTime;
             string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
             GUI.Label(rect, text, style);
+            #endregion
 
+            //Checks if user has clicked on an object
             if(GUIGameObjectShow)
             {
-                GUI.Label(new Rect(10, 20, 100, 20), hitGameObject);
-                GUI.Label(new Rect(10, 40, 100, 50), hitGameObjectTransform.position.ToString());
+                #region Getting Game Objects Name
+                GUI.Label(new Rect(10, 20, 100, 20), hitGameObjectName);
+                #endregion
 
-                // spawnPos = GUI.TextField(new Rect(10, 60, 100, 50),spawnPos.x.ToString());
+                #region Get and Set GameObject Transform
+                gameObjectTransformEdit = hitGameObjectTransform.position.ToString();
 
+                gameObjectTransformEdit = GUI.TextField(new Rect(10, 40, 100, 50), hitGameObjectTransform.position.ToString());
+
+                hitGameObjectTransform.position = StringToVector3(gameObjectTransformEdit);
+                #endregion
+
+                #region Getting Game Object Type
                 GUI.Label(new Rect(10, 60, 200, 20), gameObjectType.ToString());
-                
+                #endregion
 
-                if (GUI.Button(new Rect(10, 160, 100, 20), "Spawn " + gameObjectsToSpawn[0].name))
+                #region Adds a game Objects Name + script(s) to a list - Names are currently unique
+                foreach (MonoBehaviour mBScript in scriptsOnTarget)
                 {
-                    //Instantiate(gameObjectsToSpawn[0],new Vector3(), Quaternion.identity);
+                    if (!gameObjectList.Contains( hitGameObjectName + mBScript.GetType().Name))
+                    {
+                        gameObjectList.Add(hitGameObjectName + mBScript.GetType().Name);
+
+                        //Set type to classType
+                        classType = mBScript.GetType();
+
+                        //Get the names of the Variables
+                        foreach ( var prop in classType.GetFields())
+                            gamevariablesList.Add(hitGameObjectName + prop.Name);
+
+                        #region Activator.CreateInstance Work in Progress
+                        /*
+                        //Get class Name
+                        className = classType.Name;
+
+                        //Get the name space of the class name - needed to create an instance
+                        var nameSpace = classType.Namespace;
+
+                        //Make an instance 
+                        var myObj = Activator.CreateInstance(nameSpace, className);
+
+                        foreach (var prop in classType.GetFields())
+                        {
+                            gameVariablesValuesList.Add(prop.Name + prop.GetValue(myObj));
+                        }
+                        */
+                        #endregion
+
+                    }
+                    else
+                    {
+                        Debug.Log(hitGameObjectName + mBScript.name);
+                        
+                    }
+
                 }
+                #endregion
+
+
+
+                /*
+                ########################
+                ## List of Scripts: -> numOfScripts
+                ## (Foldout)>NameOfAllScripts in a DropDown
+                ## (foldout>> Click on script all varaibles become a drop down
+                ## All Variables that are public can have their values edited
+                ########################
+                */
+
+                #region Spawning GameObjects
+                if (gameObjectsToSpawn[0] != null)
+                {
+                    if (GUI.Button(new Rect(10, 160, 100, 20), "Spawn " + gameObjectsToSpawn[0].name))
+                    {
+                        Instantiate(gameObjectsToSpawn[0], Input.mousePosition, Quaternion.identity);
+                    }
+                }
+                #endregion
 
             }
 
--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Reflection;
 
 public class Test : MonoBehaviour {
 
     public string test = "test";
     public int testInt = 1;
+    public Type myTypetest;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start () {
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        myTypetest = typeof(Test);
+        //Debug.LogError(typeof(Test).Namespace);
+
+
+    }
 }
